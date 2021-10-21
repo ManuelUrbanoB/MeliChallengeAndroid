2be0b7008e17f3eff2dbb0f53050cce595c0c6b4
Manuel Urbano Benavides <manuel.urbanob@perficient.com>
2021-10-20 21:36:53 -0500

    feat(): attach fragment search items and create callback  - configure search view to accept taps  - Create function to review when the user tap in search, is missing implementation

--- presentation/src/main/java/com/murbanob/melichallege/presentation/ui/base/BaseActivity.kt
+++ presentation/src/main/java/com/murbanob/melichallege/presentation/ui/base/BaseActivity.kt
@@ -1,16 +1,7 @@
 package com.murbanob.melichallege.presentation.ui.base
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-open class BaseActivity: AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-    }
-
-}
+open class BaseActivity: AppCompatActivity()
--- presentation/src/main/java/com/murbanob/melichallege/presentation/ui/main/search/SearchFragment.kt
+++ presentation/src/main/java/com/murbanob/melichallege/presentation/ui/main/search/SearchFragment.kt
@@ -11,6 +11,7 @@ class SearchFragment : BaseFragment() {
 
     private var _binding: FragmentSearchItemBinding? = null
     private val binding get() = _binding
+    private var onSearchResult: ((value: String) -> Unit)? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,10 +21,30 @@ class SearchFragment : BaseFragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        changeConfigurationSearchView()
+    }
+
+    private fun changeConfigurationSearchView() {
+        binding?.searchView?.setIconifiedByDefault(true)
+        binding?.searchView?.isFocusable = true
+        binding?.searchView?.isIconified = false
+        binding?.searchView?.requestFocusFromTouch()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
+    companion object {
+        val TAG: String = SearchFragment::class.java.simpleName
+        fun createInstance(onSearchResult: ((value: String) -> Unit)): SearchFragment {
+            return SearchFragment().apply {
+                this.onSearchResult = onSearchResult
+            }
+        }
+    }
 
 }
