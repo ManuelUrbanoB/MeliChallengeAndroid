54dc28b2749df51ec4d69428a73b62fbf23c04d9
Manuel Urbano Benavides <manuel.urbanob@perficient.com>
2021-10-20 22:08:17 -0500

    provide retrofit with base url

--- app/src/main/java/com/melichallenge/di/AppModule.kt
+++ app/src/main/java/com/melichallenge/di/AppModule.kt
@@ -4,7 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.Dispatchers
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -15,7 +16,10 @@ class AppModule {
 
         @Provides
         @Singleton
-        fun provideBackgroundDispatcher() = Dispatchers.IO
+        fun provideRetrofit(): Retrofit = Retrofit.Builder()
+            .baseUrl("https://api.mercadolibre.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
     }
 }
